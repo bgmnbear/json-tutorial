3e825f471a284e1bc493472a92d4832eab034a0c
unknown <jasonwhister@gmail.com>
2017-12-11 18:59:48 +0800

    练习 2.4

--- tutorial02/leptjson.c
+++ tutorial02/leptjson.c
@@ -1,6 +1,9 @@
 #include "leptjson.h"
 #include <assert.h>  /* assert() */
+#include <errno.h>   /* errno, ERANGE */
+#include <math.h>    /* HUGE_VAL */
 #include <stdlib.h>  /* NULL, strtod() */
+
 
 #define EXPECT(c, ch)       do { assert(*c->json == (ch)); c->json++; } while(0)
 #define ISDIGIT(ch)         ((ch) >= '0' && (ch) <= '9')
@@ -47,7 +50,10 @@ static int lept_parse_number(lept_context* c, lept_value* v) {
 		if (!ISDIGIT(*p)) return LEPT_PARSE_INVALID_VALUE;
 		for (p++; ISDIGIT(*p); p++);
 	}
+	errno = 0;
 	v->n = strtod(c->json, NULL);
+	if (errno == ERANGE && (v->n == HUGE_VAL || v->n == -HUGE_VAL))
+		return LEPT_PARSE_NUMBER_TOO_BIG;
 	v->type = LEPT_NUMBER;
 	c->json = p;
 	return LEPT_PARSE_OK;
